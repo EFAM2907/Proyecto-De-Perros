c75ea590d351c66c4d2ad9c0f8efa56e70e6386e
efam2907 <efam2907@gmail.com>
2022-08-16 00:52:38 -0500

    limpiar

--- client/src/Componentes/detailDog.jsx
+++ client/src/Componentes/detailDog.jsx
@@ -49,10 +49,6 @@ export default function DetailDog() {
       <div >
      <div>
     <div className={a.margin}>
- 
-           
-            
-
             
                {
                id.includes('-')?
